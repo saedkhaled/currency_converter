23f61a91421106f32d760c4e046c93f0f7a9fac5
Saed Khaled <saedkhaled96@gmail.com>
2023-06-28 03:59:32 +0300

    Add converter provider doc comments.

--- lib/modules/currency_converter/converter_provider.dart
+++ lib/modules/currency_converter/converter_provider.dart
@@ -4,18 +4,24 @@ import 'package:http/http.dart';
 
 // An API provider for the currency converter module
 class ConverterProvider {
+  /// The base URL for the API calls
   String get baseUrl =>
       'https://v6.exchangerate-api.com/v6/${dotenv.env['CONVERTER_API_KEY']}';
+
+  /// The HTTP client to make the API calls
   final client = getIt.get<Client>();
 
+  /// Get the supported currency codes from the API
   Future<Response> getCodes() async {
     return client.get(Uri.parse('$baseUrl/codes'));
   }
 
+  /// Get the exchange rate for a currency pair
   Future<Response> getPairRate(String from, String to) async {
     return client.get(Uri.parse('$baseUrl/pair/$from/$to'));
   }
 
+  /// Get the exchange rate for a currency against all other currencies
   Future<Response> getRate(String from) async {
     return client.get(Uri.parse('$baseUrl/latest/$from'));
   }
