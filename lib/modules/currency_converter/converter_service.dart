93590e626f5ad371221883fbda19e94a39ca49a7
Saed Khaled <saedkhaled96@gmail.com>
2023-06-28 04:00:28 +0300

    Add converter service doc comments, and fix data handling.

--- lib/modules/currency_converter/converter_service.dart
+++ lib/modules/currency_converter/converter_service.dart
@@ -1,33 +1,40 @@
 import 'dart:convert';
+
 import 'package:currency_converter/main.dart';
+
 import 'converter_provider.dart';
 
+/// A service for the currency converter module that handles the business logic
+/// and communicates with the API provider
 class ConverterService {
+  /// A singleton instance of the service
   static ConverterService get to => getIt<ConverterService>();
 
+  /// The API provider to make the API calls
   late ConverterProvider provider;
 
+  /// A constructor to inject the [api] provider for testing
+  /// or use the default provider
   ConverterService({ConverterProvider? api}) {
     provider = api ?? ConverterProvider();
   }
 
-  Future<double> convert(double amount, String from, String to) async {
-    final response = await provider.getPairRate(from, to);
-    final data = jsonDecode(response.body);
-    final rate = double.parse(data['conversion_rate']);
-    return amount * rate;
-  }
-
+  /// Get the list of supported currency codes from the API
   Future<List<String>> getCodes() async {
     final response = await provider.getCodes();
     final data = jsonDecode(response.body);
-    final codes = data['supported_codes'];
+    final codes = data['supported_codes']
+        .map<String>((item) => item[0].toString())
+        .toList();
     return codes;
   }
 
-  Future<Map> getRate(String from) async {
+  /// Get the exchange rates for a currency against all other currencies
+  Future<Map<String, double>> getRates(String from) async {
     final response = await provider.getRate(from);
     final data = jsonDecode(response.body);
-    return data['conversion_rates'];
+    final rates = data['conversion_rates'].map<String, double>((key, value) =>
+        MapEntry(key.toString(), double.parse(value.toString())));
+    return rates;
   }
 }
